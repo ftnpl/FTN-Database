5722fc274d172f91df51761e182ba9d4408b7d85
R.J. Clay <jame@users.sourceforge.net>
2001-08-27 01:28:22 +0000

    Changing name from nltst.sh to nl2sql.sh
    git-svn-id: https://ftnpl.svn.sourceforge.net/svnroot/ftnpl/trunk/scripts/nodelist/nl2sql@101 987ebf5b-b7b1-44df-af0d-bd292689ea74

--- nltst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# nltst.sh - 25 August 2001 - R.J. Clay
-#  This is a test script to load a named nodelist 
-NLDIR=/var/spool/ftn/nl.d
-##./nl2sql.pl -n nodelist -d domain -x >nl2sql.out 2>nl2sql.stn.errors
-./nl2sql.pl -n $NLDIR/STNLIST.229 -d stn 2>nl2sql.stn.errors
-#
-##./nl2sql.pl -n $NLDIR/nodelist.229 -d fidonet -x 2>nl2sql.ftn.errors
-./nl2sql.pl -n $NLDIR/nodelist.229 -d fidonet 2>nl2sql.ftn.errors
