90b7e83a2e73aba07ae08b3a473aa2438af706af
R.J. Clay <jame@users.sourceforge.net>
2010-03-04 22:09:07 -0500

    Correct description & copyright in nltable.sh.

--- nltable.sh
+++ nltable.sh
@@ -1,7 +1,7 @@
 4#!/bin/sh
 # nltable.sh - 1.2
-#  This is an example test script that loads two nodelists 
-# Copyright (c) 2001-2010 Robert James Clay.  All Rights Reserved.
+#  This is an example script that creates the nodelist table. 
+# Copyright (c) 2010 Robert James Clay.  All Rights Reserved.
 # This is free software;  you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
