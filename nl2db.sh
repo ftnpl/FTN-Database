fb589f1428c592c7fee0f2770f9de0e6161a5ad3
R.J. Clay <jame@users.sourceforge.net>
2010-07-15 16:25:57 -0400

    Change nl2sql references in nl2db.sh.

--- nl2db.sh
+++ nl2db.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# nl2sql.sh - 1.2
+# nl2db.sh - 1.2
 #  This is an example test script that loads two nodelists 
 # Copyright (c) 2001-2010 Robert James Clay.  All Rights Reserved.
 # This is free software;  you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
-BINDIR=/opt/ftn/sql/nl2sql
+BINDIR=/opt/ftn/sql/nl2db
 NLDIR=/var/spool/ftn/nl.d
 LOGDIR=/var/log/bbsdbpl
-LOGFILE=$LOGDIR/nl2sql.log
-##$BINDIR/nl2sql.pl -n nldir [-f nlfile] [-l logfile] [-d domain] [-v] [-x] [-e] 2>$LOGDIR/nl2sql.stn.errors
-$BINDIR/nl2sql.pl -n $NLDIR -f STNLIST -l $LOGFILE -d stn -v -x 2>$LOGDIR/nl2sql.stn.errors
+LOGFILE=$LOGDIR/nl2db.log
+##$BINDIR/nl2db.pl -n nldir [-f nlfile] [-l logfile] [-d domain] [-v] [-x] [-e] 2>$LOGDIR/nl2db.stn.errors
+$BINDIR/nl2db.pl -n $NLDIR -f STNLIST -l $LOGFILE -d stn -v -x 2>$LOGDIR/nl2db.stn.errors
 #
-#$BINDIR/nl2sql.pl -n $NLDIR [-f nodelist] [-d fidonet] [-v] [-x] [-e] 2>$LOGDIR/nlsql.ftn.errors
-$BINDIR/nl2sql.pl -n $NLDIR $LOGFILE -d fidonet -v 2>$LOGDIR/nl2sql.ftn.errors
+#$BINDIR/nl2db.pl -n $NLDIR [-f nodelist] [-d fidonet] [-v] [-x] [-e] 2>$LOGDIR/nlsql.ftn.errors
+$BINDIR/nl2db.pl -n $NLDIR $LOGFILE -d fidonet -v 2>$LOGDIR/nl2db.ftn.errors
