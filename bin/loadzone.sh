8b87b619438313dc650dfd5482d0d7a1a02b124f
R.J. Clay <jame@users.sourceforge.net>
2010-07-21 12:40:24 -0400

    Add example script bin/loadzone.sh
    Add an example script named loadzone.sh that loads a specific zone from
    a specific nodelist file.  Uses the -z parameter to specify the zone
    number to be loaded, and uses the -f and -e parameters to specify the
    nodelist file the zone is to be loaded from.

---
+++ bin/loadzone.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# loadzone.sh - 1.2
+#  This is an example script that loads a specific zone from
+# a specific nodelist file.
+# Copyright (c) 2010 Robert James Clay.  All Rights Reserved.
+# This is free software;  you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+APPDIR=/opt/ftn/nl2db
+BINDIR=$APPDIR/bin
+LOGDIR=$APPDIR/log
+LOGFILE=$LOGDIR/nl2db.log
+NLDIR=/opt/ftn/nodelist
+NLFILE=nodelist.197
+
+cd $APPDIR
+
+#$BINDIR/nl2db.pl -n $NLDIR [-D dbname] [-u dbuser] [-p dbpass] [-f nodelist] [-d fidonet] [-v] [-x] [-e] [-z zonenum] 2>$LOGDIR/nlsql.ftn.errors
+$BINDIR/nl2db.pl -n $NLDIR -f $NLFILE -l $LOGFILE -d fidonet -z 1 -e -v 2>$LOGDIR/nl2db.ftn.errors
