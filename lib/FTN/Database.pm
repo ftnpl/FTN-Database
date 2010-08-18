6c3889960903b8752e9b5c6d508c802aee085363
R.J. Clay <jame@users.sourceforge.net>
2010-08-18 00:38:29 -0400

    Bump version of FTN::Database to v0.03
    Bump version of FTN::Database and FTN::Database::Nodelist
    modules to v0.03.

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -10,11 +10,11 @@ FTN::Database - FTN SQL Database related operations for Fidonet/FTN related proc
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
