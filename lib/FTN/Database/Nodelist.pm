d804e609df0b2a9719d0befcf1a3fc627b065419
R.J. Clay <jame@users.sourceforge.net>
2010-08-21 17:32:33 -0400

    Bump version to v0.07

--- lib/FTN/Database/Nodelist.pm
+++ lib/FTN/Database/Nodelist.pm
@@ -10,11 +10,11 @@ FTN::Database::Nodelist - Fidonet/FTN Nodelist SQL Database operations.
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 
 =head1 SYNOPSIS
