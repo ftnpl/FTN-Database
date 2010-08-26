6bea2a8ec8d762f8f562c13e8e99615493d7d43c
R.J. Clay <jame@users.sourceforge.net>
2010-08-26 15:43:13 -0400

    Add SEE ALSO pod section to documentation.

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -108,9 +108,6 @@ Please report any bugs or feature requests to C<bug-ftn-database at rt.cpan.org>
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=FTN-Database>. I will be
 notified, and then you'll automatically be notified of progress on your bug as I make changes.
 
-
-
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
@@ -140,9 +137,10 @@ L<http://search.cpan.org/dist/FTN-Database>
 
 =back
 
+=head1 SEE ALSO
 
-=head1 ACKNOWLEDGEMENTS
-
+`L<FTN::Database::Nodelist>, L<ftndbadm>, L<listftndb>, L<ftndbadm>,
+ and L<nl2ftndb>
 
 =head1 COPYRIGHT & LICENSE
 
