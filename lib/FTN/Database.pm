32a6a08f37a7b0043851e053226302f2a944ca6f
R.J. Clay <jame@users.sourceforge.net>
2010-09-28 12:57:55 -0400

    Change open_ftndb function call to using a hash reference.
    The call to the open_ftndb function changed from using four variables
    to using a referenced hash containing the items:  Type, Name, User, and
    Password.

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -53,27 +53,29 @@ sub create_ftndb {
 
 =head2 open_ftndb
 
-Syntax:  $db_handle = open_ftndb($db_type, $db_name, $db_user, $db_pass);
+Syntax:  $db_handle = open_ftndb(\%db_options);
 
-Open a database for Fidonet/FTN processing, where:
+Open a database for Fidonet/FTN processing, where the referenced hash
+contains the following items:
 
 =over
 
-=item	$db_type
+=item	Type
 
 The database type.  This needs to be a database type for which 
 a DBD module exists, the type being the name as used in the DBD
 module.  The default type to be used is SQLite.
 
-=item	$db_name
+=item	Name
 
-The database name.
+The name of the database to be opened.  If the Type is SQLite, this
+is a filename and path to the database file.
 
-=item	$db_user
+=item	User
 
 The database user, which should already have the neccesary priviledges.
 
-=item	$db_pass
+=item	Password
 
 The database password for the database user.
 
@@ -89,9 +91,13 @@ sub open_ftndb {
 
     use DBI;
 
-    my($db_type, $db_name, $db_user, $db_pass) = @_;
+    # Get the hash reference to the information required for the connect
+    my $option = @_;
 
-    ( my $db_handle = DBI->connect( "dbi:$db_type:dbname=$db_name", $db_user, $db_pass ) )
+    ( my $db_handle = DBI->connect(
+    		"dbi:${$option}{'Type'}:dbname=${$option}{'Name'}",
+    		${$option}{'User'},
+    		${$option}{'Password'} ) )
 	or croak($DBI::errstr);
 
     return($db_handle);
