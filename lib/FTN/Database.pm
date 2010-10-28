2025b702eb6b5d7175f414652f0d2bd55ac7a3e2
R.J. Clay <jame@users.sourceforge.net>
2010-10-28 19:55:00 -0400

    Bump version to v0.21

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -10,11 +10,11 @@ FTN::Database - FTN SQL Database related operations for Fidonet/FTN related proc
 
 =head1 VERSION
 
-Version 0.20
+Version 0.21
 
 =cut
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 =head1 DESCRIPTION
 
