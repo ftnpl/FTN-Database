aaa0f08103dedcffee2592a9fa4ed0ebfae8f2e1
RJ Clay <jame@rocasa.us>
2011-07-17 10:47:04 -0400

    Update copyright year information.

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -237,7 +237,7 @@ up to the version at which they were last changed.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2010 Robert James Clay, all rights reserved.
+Copyright 2010-2011 Robert James Clay, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
