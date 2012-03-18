efea9030ed4cae17bfa2c3c387d7b65450a6e7b8
RJ Clay <jame@rocasa.us>
2012-03-18 10:10:16 -0400

    Change the 'phone' field constraint from 20 to 32.

--- lib/FTN/Database/Nodelist.pm
+++ lib/FTN/Database/Nodelist.pm
@@ -55,7 +55,7 @@ sub create_nodelist_table {
     $sql_statement .= "name      VARCHAR(48) DEFAULT '' NOT NULL, ";
     $sql_statement .= "location  VARCHAR(48) DEFAULT '' NOT NULL, ";
     $sql_statement .= "sysop     VARCHAR(48) DEFAULT '' NOT NULL, ";
-    $sql_statement .= "phone     VARCHAR(20) DEFAULT '000-000-000-000' NOT NULL, ";
+    $sql_statement .= "phone     VARCHAR(32) DEFAULT '000-000-000-000' NOT NULL, ";
     $sql_statement .= "baud      CHAR(6) DEFAULT '300' NOT NULL, ";
     $sql_statement .= "flags     VARCHAR(128) DEFAULT ' ' NOT NULL, ";
     $sql_statement .= "domain    VARCHAR(8) DEFAULT 'fidonet' NOT NULL, ";
