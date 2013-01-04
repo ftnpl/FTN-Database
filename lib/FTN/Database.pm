65985a3d4210f63149022ba2705c8573f173c8ab
RJ Clay <jame@rocasa.us>
2013-01-04 08:12:15 -0500

    Bump version to v0.40

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -10,11 +10,11 @@ FTN::Database - FTN SQL Database related operations for Fidonet/FTN related proc
 
 =head1 VERSION
 
-Version 0.39
+Version 0.40
 
 =cut
 
-our $VERSION = '0.39';
+our $VERSION = '0.40';
 
 =head1 DESCRIPTION
 
