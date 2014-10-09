30ee61f67126a555a1b472b1c1915705a1718b00
RJ Clay <jame@rocasa.us>
2014-10-09 03:55:01 -0400

    Bump version to v0.42

--- lib/FTN/Database/Nodelist.pm
+++ lib/FTN/Database/Nodelist.pm
@@ -10,11 +10,11 @@ FTN::Database::Nodelist - Fidonet/FTN Nodelist SQL Database operations.
 
 =head1 VERSION
 
-Version 0.41
+Version 0.42
 
 =cut
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 =head1 DESCRIPTION
 
